6b1528d0c4545e87e0b7c09e10aebfcc00960568
Mayukhdevan <mayukhdevansahu121@gmail.com>
2023-12-26 23:55:39 +0530

    login, logout, login form validation, next auth

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -4,10 +4,10 @@ import { lusitana } from '@/app/ui/fonts';
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white gap-2`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">MDS</p>
+      <p className="text-[24px]">Dashy</p>
     </div>
   );
 }
